e76c3ac67fcd76f3b7f7ebdf8ae018595f1ae8f7
Shubham <ssharma0834@gmail.com>
2018-11-20 23:17:24 +0530

    Updated Bootstrap

---
+++ TheWebKing/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TheWebKing.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult UserLogin()
+        {
+            return View();
+        }
+    }
+}
---
+++ TheWebKing/Scripts/Card JS/ChangePage.js
@@ -0,0 +1,25 @@
+﻿const changePage = function (e) {
+    const register = document.getElementById('register');
+    const login = document.getElementById('login');
+    const forgot = document.getElementById('forgot-password');
+    if (e === 'forgot-password') {
+        forgot.classList.contains('card-hidden') ?
+            forgot.classList.remove('card-hidden') : forgot.classList.add('card-hidden');
+        login.classList.contains('card-hidden') ?
+            login.classList.remove('card-hidden') : login.classList.add('card-hidden');
+    }
+    else if (e === 'register') {
+        register.classList.contains('card-hidden') ?
+            register.classList.remove('card-hidden') : register.classList.add('card-hidden');
+        login.classList.contains('card-hidden') ?
+            login.classList.remove('card-hidden') : login.classList.add('card-hidden');
+    }
+}
+
+$('.toggle').on('click', function () {
+    $('.container').stop().addClass('active');
+});
+
+$('.close').on('click', function () {
+    $('.container').stop().removeClass('active');
+});
